e0ca8afd4882ca71b026c2b979fc24b2e8b20f35
Corey Gray <csgray@alaska.edu>
2018-02-25 17:27:23 -0900

    Move ships into a game group.

--- docs/js/pre.js
+++ docs/js/pre.js
@@ -23,20 +23,19 @@ var preState = {
         text = game.add.text(16, 16, 'Drag the sprites. Overlapping: false', { fill: '#ffffff' });
         hit = game.add.sprite(135, 157, 'hit');
         miss = game.add.sprite(180, 157, 'miss');
-        
-        ships =[carrier = game.add.sprite(705, 157, 'carrier'),
-                battleship = game.add.sprite(750, 157, 'battleship'),
-                cruiser = game.add.sprite(795, 157, 'cruiser'),
-                submarine = game.add.sprite(840, 157, 'submarine'),
-                destroyer = game.add.sprite(885, 157, 'destroyer')
-                ];
 
-        shipBounds = new Phaser.Rectangle(705, 157, 450, 450);
+        ships = game.add.group();
+
+        carrier = ships.create(705, 157, 'carrier'),
+        battleship = ships.create(750, 157, 'battleship'),
+        cruiser = ships.create(795, 157, 'cruiser'),
+        submarine = ships.create(840, 157, 'submarine'),
+        destroyer = ships.create(885, 157, 'destroyer')
+
         ships.forEach(function(element) {
             element.inputEnabled = true;
             element.input.enableDrag();
             element.input.enableSnap(45,45,false,true, 30, 22);
-            element.input.boundsRect = shipBounds;
             element.events.onInputDown.add(preState.doubleTap, this);
         });
     },
